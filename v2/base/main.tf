06c8c7e6f57061ed041232c186fcc2cfe6026920
sarumont <richard@sigil.org>
2023-07-12 09:17:31 -0600

    feat: add traefik

--- v2/base/main.tf
+++ v2/base/main.tf
@@ -10,3 +10,17 @@ resource "helm_release" "metallb" {
   chart      = "metallb"
   namespace  = "metallb-system"
 }
+
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = "traefik"
+  }
+}
+
+resource "helm_release" "traefik" {
+  name       = "traefik"
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+  version    = var.traefix_chart_version
+  namespace  = kubernetes_namespace.traefik.metadata.0.name
+}
---
+++ v2/base/variables.tf
@@ -0,0 +1,8 @@
+variable "traefix_chart_version" {
+  description = "Version of the Traefix Helm chart to use"
+  default = "23.1.0"
+}
+
+variable "cluster_domain" {
+  description = "Domain to use for cluster DNS names"
+}
