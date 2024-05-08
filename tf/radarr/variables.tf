7ace69e4a6cabd0eabe0335f7544825b13110844
Richard Kolkovich <richard@sigil.org>
2024-05-08 07:56:12 -0600

    chore: clean up scar tissue

--- tf/radarr/variables.tf
+++ tf/radarr/variables.tf
@@ -32,35 +32,3 @@ variable "nfs_volumes" {
     })
   )
 }
-
-
-# variable "image_version" {
-#   description = "Version of the Radarr docker image use"
-#   default = "1.32.6.7468-07e0d4a7e"
-# }
-#
-# variable "access_mode" {
-#   description = "Access mode for the NFS share (see Kubernetes PersistentVolume)"
-#   type    = string
-#   default = "ReadWriteMany"
-#
-#   validation {
-#     condition     = contains(["ReadWriteMany", "ReadOnlyMany", "ReadWriteOnce"], var.access_mode)
-#     error_message = "Allowed values for access_mode are \"ReadWriteMany\", \"ReadOnlyMany\", or \"ReadWriteOnce\"."
-#   }
-# }
-#
-# variable "ip" {
-#   description = "IP address to assign to Plex"
-# }
-#
-# variable "nfs_volumes" {
-#   description = "NFS volumes to mount"
-#   type = list(
-#     object({
-#       name   = string,
-#       server = string,
-#       path   = string,
-#     })
-#   )
-# }
