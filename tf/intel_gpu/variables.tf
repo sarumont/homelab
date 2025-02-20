d0d28073a747ab481445e9fc79727c9f7433f799
Richard Kolkovich <richard@sigil.org>
2025-02-20 11:20:06 -0700

    chore(deps): bump Intel Device Operator chart version

--- tf/intel_gpu/variables.tf
+++ tf/intel_gpu/variables.tf
@@ -1,11 +1,11 @@
 variable "operator_chart_version" {
   description = "Version of the Intel Device Plugin Operator Helm chart to use"
-  default = "0.27.1"
+  default = "0.32.0"
 }
 
 variable "device_plugins_chart_version" {
   description = "Version of the Intel GPU Helm chart to use"
-  default = "0.27.1"
+  default = "0.32.0"
 }
 
 variable "shared_device_number" {
