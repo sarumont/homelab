bfb8879fade9bd26ccd57e797271d47c1ac72633
Richard Kolkovich <richard@sigil.org>
2026-03-10 14:00:26 -0600

    feat: add DB backup module

---
+++ tf/db_backup/main.tf
@@ -0,0 +1,97 @@
+locals {
+  pg_image      = "postgres:16-alpine"
+  mariadb_image = "mariadb:10.11"
+
+  commands = { for k, v in var.backups : k =>
+    v.db_type == "postgres" ? join(" && ", [
+      "mkdir -p /backup/${v.namespace}",
+      "PGPASSWORD=\"$DB_PASSWORD\" pg_dumpall -h ${v.db_host} -p ${v.db_port} -U \"$DB_USER\" | gzip > /backup/${v.namespace}/${k}-$(date +%Y%m%d-%H%M%S).sql.gz",
+      "find /backup/${v.namespace} -name '${k}-*.sql.gz' -mtime +${var.retention_days} -delete",
+      ]) : join(" && ", [
+      "mkdir -p /backup/${v.namespace}",
+      "mysqldump -h ${v.db_host} -P ${v.db_port} -u \"$DB_USER\" -p\"$DB_PASSWORD\" --all-databases | gzip > /backup/${v.namespace}/${k}-$(date +%Y%m%d-%H%M%S).sql.gz",
+      "find /backup/${v.namespace} -name '${k}-*.sql.gz' -mtime +${var.retention_days} -delete",
+    ])
+  }
+}
+
+resource "kubernetes_cron_job_v1" "backup" {
+  for_each = var.backups
+
+  metadata {
+    name      = "${each.key}-backup"
+    namespace = each.value.namespace
+  }
+
+  spec {
+    schedule                      = each.value.schedule
+    successful_jobs_history_limit = 3
+    failed_jobs_history_limit     = 1
+    concurrency_policy            = "Forbid"
+
+    job_template {
+      metadata {}
+
+      spec {
+        template {
+          metadata {}
+
+          spec {
+            restart_policy = "OnFailure"
+
+            container {
+              name    = "backup"
+              image   = each.value.db_type == "postgres" ? local.pg_image : local.mariadb_image
+              command = ["/bin/sh", "-c"]
+              args    = [local.commands[each.key]]
+
+              # DB_USER: from db_user variable if set, otherwise from secret
+              dynamic "env" {
+                for_each = each.value.db_user != null ? [1] : []
+                content {
+                  name  = "DB_USER"
+                  value = each.value.db_user
+                }
+              }
+              dynamic "env" {
+                for_each = each.value.db_user == null ? [1] : []
+                content {
+                  name = "DB_USER"
+                  value_from {
+                    secret_key_ref {
+                      name = each.value.secret_name
+                      key  = each.value.secret_username_key
+                    }
+                  }
+                }
+              }
+
+              env {
+                name = "DB_PASSWORD"
+                value_from {
+                  secret_key_ref {
+                    name = each.value.secret_name
+                    key  = each.value.secret_password_key
+                  }
+                }
+              }
+
+              volume_mount {
+                name       = "backup-storage"
+                mount_path = "/backup"
+              }
+            }
+
+            volume {
+              name = "backup-storage"
+              nfs {
+                server = var.nfs_server
+                path   = var.nfs_path
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ tf/db_backup/variables.tf
@@ -0,0 +1,33 @@
+variable "backups" {
+  description = "Map of database backup definitions"
+  type = map(object({
+    namespace           = string
+    schedule            = optional(string, "0 2 * * *")
+    db_type             = string
+    db_host             = string
+    db_port             = optional(number, 5432)
+    secret_name         = string
+    db_user             = optional(string, null)
+    secret_username_key = optional(string, "username")
+    secret_password_key = optional(string, "password")
+  }))
+
+  validation {
+    condition     = alltrue([for k, v in var.backups : contains(["postgres", "mariadb"], v.db_type)])
+    error_message = "db_type must be \"postgres\" or \"mariadb\"."
+  }
+}
+
+variable "nfs_server" {
+  description = "NFS server IP"
+}
+
+variable "nfs_path" {
+  description = "NFS base path for backups"
+}
+
+variable "retention_days" {
+  description = "Number of days to retain backups"
+  default     = 7
+  type        = number
+}
